d2f6354caba75b5d9bb0b973fdf63ac9031239f2
Yacob2212 <100694865+Yacob2212@users.noreply.github.com>
2022-03-07 13:21:18 +0100

    Add files via upload

---
+++ Bijen.py
@@ -0,0 +1,23 @@
+<h1>Bijen</h1>
+
+<h2>Voeding</h2>
+
+<p>Bijen leven van nectar, van het stuifmeel van bloemen en van andere zoete afscheidingen zoals honingdauw. De honing is de overwinteringsvoeding voor de honingbijen. De larven worden grootgebracht met voedersappen. Voedersappen worden door voedsterbijen geproduceerd en zijn een mengsel van eiwitrijke lichaamseigen afscheidingen van de voedersapklieren, nectar en stuifmeel.</p>
+
+<p>Honing is voor de bijen hun wintervoorraad. Imkers nemen deze (deels) weg en geven de bijen daarvoor suikerwater als voedsel.</p>
+
+<h2>Warmte als verdedigingsmiddel</h2>
+
+<p>Honingbijen kunnen hun borststuk of middensegment warmte laten uitstralen door de vliegspieren heel snel te laten trillen. Wanneer twaalf of meer bijen dit tegelijk doen, verhoogt dit de omgevingstemperatuur significant. Honingbijen gebruiken dit als verdedigingsmiddel tegen onder andere hoornaars. Dat zijn roodogige wespen die tot 4 cm lang kunnen worden.</p>
+
+<p>Hoornaars zijn groter en sterker dan een enkele honingbij en kunnen kleinere insecten gemakkelijk met hun krachtige kaken verscheuren. Bij aanhoudende aanvallen door hoornaars zwermen steeds meer bijen samen bij de ingang van hun nest. Zodra dan een hoornaar te dichtbij komt wordt deze door een groep bijen besprongen. De bijen bedekken gezamenlijk de hoornaar en laten hun vliegspieren dan razendsnel trillen om warmte te genereren. Hierdoor stijgt de lichaamstemperatuur van de hoornaar tot het punt dat deze bezwijkt door de hitte. De bijen die in het binnenste van zo'n 'warmteval' om de hoornaar zitten offeren zichzelf daarbij soms op ter bescherming van de kolonie.</p>
+
+<h2>Ecologische betekenis</h2>
+
+<button>"I like this"</button>
+
+<button>"I'm neutral"</button>
+
+<button>"I'm not interested"</button>
+
+
